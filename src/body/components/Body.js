0968b2256e492cbd8f06d438846cc5e527386a1f
Victor Sanchez <vsanchez.mail@gmail.com>
2018-08-15 15:41:39 -0300

    Markup header

--- src/body/components/Body.js
+++ src/body/components/Body.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Divider, Card, Row, Col } from 'antd'
+
+import './Body.css'
 
 const Content = Layout.Content
 
@@ -7,7 +9,49 @@ export default class Body extends Component {
   render() {
     return (
       <Content style={{ padding: '0 50px' }}>
-        <div style={{ background: '#fff', padding: 24, height: '100%' }}>Content</div>
+        <div style={{ background: '#fff', padding: 24 }}>
+          <div>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">85%</p>
+                  <p className="description">Conectados</p>
+                </Card.Grid>
+              </Col>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">20%</p>
+                  <p className="description">Remoto</p>
+                </Card.Grid>
+              </Col>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">10%</p>
+                  <p className="description">AFK</p>
+                </Card.Grid>
+              </Col>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">10%</p>
+                  <p className="description">AFK</p>
+                </Card.Grid>
+              </Col>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">10%</p>
+                  <p className="description">AFK</p>
+                </Card.Grid>
+              </Col>
+              <Col className="gutter-row" span={4}>
+                <Card.Grid style={{ width: '100%' }}>
+                  <p className="text">10%</p>
+                  <p className="description">AFK</p>
+                </Card.Grid>
+              </Col>
+            </Row>
+            <Divider />
+          </div>
+        </div>
       </Content>
     )
   }
