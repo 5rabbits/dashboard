45b22e51d6e247141c47b0c00fae8a58817be16b
Victor Sanchez <vsanchez.mail@gmail.com>
2018-08-16 00:03:33 -0300

    Add commits charts

--- src/body/components/Body.js
+++ src/body/components/Body.js
@@ -1,11 +1,121 @@
 import React, { Component } from 'react'
-import { Layout, Divider, Card, Row, Col } from 'antd'
+import { Layout, Divider, Card, Row, Col, Table } from 'antd'
+import Chart from 'react-google-charts'
 
 import './Body.css'
 
 const Content = Layout.Content
+const Meta = Card.Meta
 
 export default class Body extends Component {
+
+  commitsChart = () => {
+    const columns = [
+      {
+        type: "string",
+        label: "Fecha"
+      },
+      {
+        label: "TBX",
+        type: "number"
+      },
+      {
+        label: "TTB",
+        type: "number"
+      },
+      {
+        label: "CT",
+        type: "number"
+      },
+      {
+        label: "IPM",
+        type: "number"
+      }
+    ]
+    const rows = [
+      ['15-08', 100, 120, 98, 50],
+      ['16-08', 98, 100, 110, 45],
+      ['17-08', 110, 105, 80, 37],
+      ['18-08', 78, 123, 105, 60],
+    ]
+
+    return (
+      <Chart
+        chartType="AreaChart"
+        width="100%"
+        height="400px"
+        legendToggle
+        rows={rows}
+        columns={columns}
+      />
+    )
+  }
+
+  dataSourceTableCommit = () => {
+    return ([
+      {
+        key: 1,
+        name: 'Victor Sanchez',
+        commit: 'Test 12321',
+        project: 'TimeBillingX',
+        date: '17/08/2018'
+      },
+      {
+        key: 2,
+        name: 'Victor Sanchez',
+        commit: 'Test 12321',
+        project: 'TimeBillingX',
+        date: '17/08/2018'
+      },
+      {
+        key: 3,
+        name: 'Victor Sanchez',
+        commit: 'Test 12321',
+        project: 'TimeBillingX',
+        date: '17/08/2018'
+      },
+      {
+        key: 4,
+        name: 'Victor Sanchez',
+        commit: 'Test 12321',
+        project: 'TimeBillingX',
+        date: '17/08/2018'
+      },
+      {
+        key: 5,
+        name: 'Victor Sanchez',
+        commit: 'Test 12321',
+        project: 'TimeBillingX',
+        date: '17/08/2018'
+      },
+    ])
+  }
+
+  columnsTableCommit = () => {
+    return ([
+      {
+        title: 'Nombre',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Commit',
+        dataIndex: 'commit',
+        key: 'date',
+      },
+      {
+        title: 'Proyecto',
+        dataIndex: 'project',
+        key: 'project',
+      },
+      {
+        title: 'Date',
+        dataIndex: 'date',
+        key: 'date',
+      },
+    ])
+  }
+
   render() {
     return (
       <Content style={{ padding: '0 50px' }}>
@@ -50,6 +160,19 @@ export default class Body extends Component {
               </Col>
             </Row>
             <Divider />
+            <Row gutter={16}>
+              <Col className="gutter-row" span={16}>
+                <Card
+                  hoverable
+                  cover={this.commitsChart()}
+                >
+                  <Table
+                    dataSource={this.dataSourceTableCommit()}
+                    columns={this.columnsTableCommit()}
+                  />
+                </Card>
+              </Col>
+            </Row>
           </div>
         </div>
       </Content>
